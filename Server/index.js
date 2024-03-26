00f09977329fdda0cc010780acd60e8b17bf59af
dhanesh1232 <dhaneshreddy980@gmail.com>
2024-03-26 17:11:19 +0530

    Payload Error added

--- Server/index.js
+++ Server/index.js
@@ -15,6 +15,8 @@ const accountData = require("./routes/account");
 app.use(express.json());
 app.use(cors());
 
+app.use(express.json({ limit: '10mb' })); 
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   next();
