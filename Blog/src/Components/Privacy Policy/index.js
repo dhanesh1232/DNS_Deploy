97b6080b7851234ff996f87889e609078f4ad5d8
dhanesh1232 <75924227+dhanesh1232@users.noreply.github.com>
2024-05-24 13:55:26 +0530

    Update index.js

--- Blog/src/Components/Privacy Policy/index.js
+++ Blog/src/Components/Privacy Policy/index.js
@@ -1,7 +1,21 @@
 import BlogContext from "../../Context/blogContext";
 import Header from "../Header";
 import Footer from "../Footer";
-import { PrivacyPage, PrivacyBody } from "./styledComponents";
+import {
+  PrivacyPage,
+  PrivacyBody,
+  PrivacyHead,
+  MiniHead,
+  DateLine,
+  FirstStrong,
+  SecondStrong,
+  DescriptionCon,
+  DescUnorderList,
+  DescList,
+  FirstHead,
+  SecondHead,
+  NavLinkPrivacy,
+} from "./styledComponents";
 const PrivacyPolicy = () => (
   <BlogContext.Consumer>
     {(value) => {
@@ -10,8 +24,168 @@ const PrivacyPolicy = () => (
         <>
           <Header />
           <PrivacyPage>
-            <PrivacyBody headcolor={theme.toString()}>
-              <h1>Privacy Policy</h1>
+            <PrivacyHead headcolor={theme.toString()}>
+              Privacy Policy
+            </PrivacyHead>
+            <PrivacyBody>
+              <MiniHead minicolor={theme.toString()}>
+                Privacy Policy for ECO DROP
+              </MiniHead>
+              <DateLine>
+                <FirstStrong firststrongcolor={theme.toString()}>
+                  Effictive Date:
+                </FirstStrong>
+                <SecondStrong secondstrongcolor={theme.toString()} mrg="0px">
+                  01/02/2024
+                </SecondStrong>
+              </DateLine>
+              <SecondStrong secondstrongcolor={theme.toString()} mrg="15px">
+                At ECO DROP, we are committed to protecting your privacy. This
+                Privacy Policy explains how we collect, use, disclose, and
+                safeguard your information when you visit our website
+                www.ecodrop.com.
+              </SecondStrong>
+              <DescriptionCon>
+                <FirstHead firstheadcolor={theme.toString()}>
+                  1. Information We Collect
+                </FirstHead>
+                <DescUnorderList>
+                  <DescList>
+                    <SecondHead secondheadcolor={theme.toString()}>
+                      Personal Data:{" "}
+                    </SecondHead>
+                    <SecondStrong
+                      secondstrongcolor={theme.toString()}
+                      mrg="5px"
+                    >
+                      When you visit our site, we may collect personally
+                      identifiable information, such as your name, email
+                      address, and contact details.
+                    </SecondStrong>
+                  </DescList>
+                  <DescList>
+                    <SecondHead secondheadcolor={theme.toString()}>
+                      Usage Data:
+                    </SecondHead>
+                    <SecondStrong
+                      secondstrongcolor={theme.toString()}
+                      mrg="5px"
+                    >
+                      We may collect information about your interactions with
+                      our website, such as IP addresses, browser type, and pages
+                      visited.
+                    </SecondStrong>
+                  </DescList>
+                </DescUnorderList>
+              </DescriptionCon>
+              <DescriptionCon>
+                <FirstHead firstheadcolor={theme.toString()}>
+                  2. How We Use Your Information
+                </FirstHead>
+                <DescUnorderList>
+                  <DescList>
+                    <SecondStrong
+                      secondstrongcolor={theme.toString()}
+                      mrg="5px"
+                    >
+                      To provide and maintain our service.
+                    </SecondStrong>
+                  </DescList>
+                  <DescList>
+                    <SecondStrong
+                      secondstrongcolor={theme.toString()}
+                      mrg="5px"
+                    >
+                      To notify you about changes to our service.
+                    </SecondStrong>
+                  </DescList>
+                  <DescList>
+                    <SecondStrong
+                      secondstrongcolor={theme.toString()}
+                      mrg="5px"
+                    >
+                      To allow you to participate in interactive features.
+                    </SecondStrong>
+                  </DescList>
+                  <DescList>
+                    <SecondStrong
+                      secondstrongcolor={theme.toString()}
+                      mrg="5px"
+                    >
+                      To provide customer support.
+                    </SecondStrong>
+                  </DescList>
+                  <DescList>
+                    <SecondStrong
+                      secondstrongcolor={theme.toString()}
+                      mrg="5px"
+                    >
+                      To gather analysis to improve our service.
+                    </SecondStrong>
+                  </DescList>
+                </DescUnorderList>
+              </DescriptionCon>
+              <DescriptionCon>
+                <FirstHead firstheadcolor={theme.toString()}>
+                  3. Sharing Your Information
+                </FirstHead>
+                <DescUnorderList>
+                  <DescList>
+                    <SecondStrong
+                      secondstrongcolor={theme.toString()}
+                      mrg="5px"
+                    >
+                      We do not sell, trade, or otherwise transfer your personal
+                      information to outside parties without your consent,
+                      except for trusted partners who assist us in operating our
+                      website and conducting our business.
+                    </SecondStrong>
+                  </DescList>
+                </DescUnorderList>
+              </DescriptionCon>
+              <DescriptionCon>
+                <FirstHead firstheadcolor={theme.toString()}>
+                  4. Security of Your Information
+                </FirstHead>
+                <DescUnorderList>
+                  <DescList>
+                    <SecondStrong
+                      secondstrongcolor={theme.toString()}
+                      mrg="5px"
+                    >
+                      We use administrative, technical, and physical security
+                      measures to protect your personal information.
+                    </SecondStrong>
+                  </DescList>
+                </DescUnorderList>
+              </DescriptionCon>
+              <DescriptionCon>
+                <FirstHead firstheadcolor={theme.toString()}>
+                  5. Changes to This Privacy Policy
+                </FirstHead>
+                <DescUnorderList>
+                  <DescList>
+                    <SecondStrong
+                      secondstrongcolor={theme.toString()}
+                      mrg="5px"
+                    >
+                      We may update our Privacy Policy from time to time. We
+                      will notify you of any changes by posting the new Privacy
+                      Policy on this page.
+                    </SecondStrong>
+                  </DescList>
+                </DescUnorderList>
+              </DescriptionCon>
+              <FirstHead firstheadcolor={theme.toString()}>
+                Contact Us
+              </FirstHead>
+              <SecondStrong secondstrongcolor={theme.toString()} mrg="5px">
+                If you have any questions about this Privacy Policy, please
+                contact us at{" "}
+                <NavLinkPrivacy secondstrongcolor={theme.toString()}>
+                  Click Here
+                </NavLinkPrivacy>
+              </SecondStrong>
             </PrivacyBody>
             <Footer />
           </PrivacyPage>
