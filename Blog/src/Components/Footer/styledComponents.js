44da2ee183e775cb30bb7889f31b0dd267b64ae2
dhanesh1232 <75924227+dhanesh1232@users.noreply.github.com>
2024-05-24 13:32:38 +0530

    Update styledComponents.js

--- Blog/src/Components/Footer/styledComponents.js
+++ Blog/src/Components/Footer/styledComponents.js
@@ -3,14 +3,16 @@ import { Link } from "react-router-dom";
 
 export const FooterPage = styled.div`
   width: 100%;
-  background: #8080802e;
   margin-top: 15px;
+  background: ${(props) =>
+    JSON.parse(props.footerbg) ? "#262121" : "#f3f3f3"};
   font-family: "Roboto";
 `;
 export const FooterCon = styled.div`
   width: 100%;
   display: flex;
   padding: 15px;
+  gap: 10px;
   color: ${(props) => (JSON.parse(props.copycolor) ? "#fff" : "#262626")};
 `;
 export const PoliciesCon = styled.div`
@@ -40,4 +42,11 @@ export const NavLinkFooter = styled(Link)`
   &:hover {
     color: blue;
   }
+`;
+export const SliderTextNav = styled(Link)`
+  font-size: 15px;
+  font-weight: 500;
+  transition: all 0.3s ease-in-out;
+  cursor: pointer;
+  color: ${(props) => (JSON.parse(props.linkcolor) ? "#fff" : "#262626")};
 `;
